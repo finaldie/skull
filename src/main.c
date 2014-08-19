42fbbe01a12ff99b68a2413f7d2f9ebeb5e1c4f4
Yuzhang Hu <hyzwowtools@gmail.com>
2014-08-19 08:57:56 +0000

    add _check_args()

--- src/main.c
+++ src/main.c
@@ -35,11 +35,22 @@ void _read_commands(int argc, char** argv, skull_cmd_args_t* cmd_args)
     }
 }
 
+static
+void _check_args(skull_cmd_args_t* cmd_args)
+{
+    if (NULL == cmd_args->config_location) {
+        fprintf(stderr, "empty configuraton\n");
+        _print_usage();
+        exit(2);
+    }
+}
+
 int main(int argc, char** argv)
 {
     skull_core_t core;
     memset(&core, 0, sizeof(core));
     _read_commands(argc, argv, &core.cmd_args);
+    _check_args(&core.cmd_args);
 
     skull_init(&core);
     skull_start(&core);
