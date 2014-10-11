2487c5b103613151683fc02c1ecb1b6d569be34b
Yuzhang Hu <hyzwowtools@gmail.com>
2014-10-11 04:20:50 +0000

    fix the skull start action script

--- scripts/lib/skull_action_start.bash
+++ scripts/lib/skull_action_start.bash
@@ -5,34 +5,6 @@
 
 function action_start()
 {
-    local start_mode=$1
-    local skull_conf=$SKULL_PROJ_ROOT/config/skull-config.yaml
-    local deploy_dir=
-
-    if [ $start_mode = "dev" ]; then
-        deploy_dir=$SKULL_PROJ_ROOT/run
-    elif [ $start_mode = "prod" ]; then
-        echo "Fatal: Un-implemented mode"
-        exit 1
-    else
-        echo "Fatal: Unknown start_mode $start_mode"
-        exit 1
-    fi
-
-    # Fork a process to do the deployment, since we will change the working
-    # dir, this will be easily to handle the cleanup job
-    (
-        if [ $start_mode = "dev" ]; then
-            action_deploy dev
-
-            cd $deploy_dir
-            skull-engine -c $skull_conf
-        elif [ $start_mode = "prod" ]; then
-            echo "Fatal: Un-implemented mode"
-            exit 1
-        else
-            echo "Fatal: Unknown start_mode $start_mode"
-            exit 1
-        fi
-    )
+    deploy_dir=$SKULL_PROJ_ROOT/run
+    skull-engine -c $deploy_dir/skull-config.yaml
 }
