9bbe0cd3a8fd1a573ad5abd0798cf23308197015
Yuzhang Hu <hyzwowtools@gmail.com>
2015-01-25 13:57:45 +0000

    Fix the LD_LIBRARY_PATH for skull-start.sh

--- scripts/share/bin/skull-start.sh
+++ scripts/share/bin/skull-start.sh
@@ -7,8 +7,10 @@ fi
 
 # 1. Put some custom settings/actions here...
 
-# 2. Start skull engine
+# 2. Rrepare the environment variables
 skull_config=$1
-export LD_LIBRARY_PATH=`pwd`/lib
+skull_rundir=`dirname $skull_config`
+export LD_LIBRARY_PATH=$skull_rundir/lib
 
+# 3. Start skull engine
 skull-engine -c $skull_config
