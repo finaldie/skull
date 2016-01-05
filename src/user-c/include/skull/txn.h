5da65973408f22fa4feb6787ce4381d62ed559a8
Yuzhang Hu <hyzwowtools@gmail.com>
2016-01-05 21:39:26 +0000

    Add user-c api skull_txn_status()

--- src/user-c/include/skull/txn.h
+++ src/user-c/include/skull/txn.h
@@ -11,5 +11,13 @@ const char* skull_txn_idlname(skull_txn_t* txn);
 // get idl data
 void* skull_txn_idldata(skull_txn_t* txn);
 
+typedef enum skull_txn_status_t {
+    SKULL_TXN_OK = 0,
+    SKULL_TXN_ERROR
+} skull_txn_status_t;
+
+// get txn status
+skull_txn_status_t skull_txn_status(skull_txn_t* txn);
+
 #endif
 
