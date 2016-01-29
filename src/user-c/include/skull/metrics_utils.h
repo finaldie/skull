5908ad51d2ca2120ff5e391d166b6d7ab1ff108e
Yuzhang Hu <hyzwowtools@gmail.com>
2016-01-29 09:09:26 +0000

    remove useless method of skull_metrics

--- src/user-c/include/skull/metrics_utils.h
+++ src/user-c/include/skull/metrics_utils.h
@@ -4,8 +4,5 @@
 void skull_metric_inc(const char* name, double value);
 double skull_metric_get(const char* name);
 
-typedef void (*skull_metric_each)(const char* name, double value, void* ud);
-void skull_metric_foreach(skull_metric_each, void* ud);
-
 #endif
 
