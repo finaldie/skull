211330c97a7e204741a08576a9fe429f0543b9ae
Yuzhang Hu <hyzwowtools@gmail.com>
2016-02-26 13:12:04 +0000

    Refine api

--- src/user-c/include/skull/ep.h
+++ src/user-c/include/skull/ep.h
@@ -44,8 +44,7 @@ typedef void (*skull_ep_cb_t) (skull_service_t*, skull_ep_ret_t,
 
 skull_ep_status_t
 skull_ep_send(skull_service_t*, const skull_ep_handler_t handler,
-              const void* data, size_t count,
-              const skull_ep_cb_t cb, void* ud);
+              const void* data, size_t count, skull_ep_cb_t cb, void* ud);
 
 #endif
 
