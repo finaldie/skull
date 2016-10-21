71a920834ee0e56ffa7964a4098e166827ab56fb
Yuzhang Hu <hyzwowtools@gmail.com>
2016-10-21 07:22:14 +0000

    Refine skull_version output format

--- scripts/lib/skull_action_version.bash
+++ scripts/lib/skull_action_version.bash
@@ -6,16 +6,15 @@
 function action_version()
 {
     local changelog=$SKULL_ROOT/etc/skull/ChangeLog.md
-    local build_date=`cat $changelog | head -1 | awk '{print $2}'`
     local version=`cat $changelog | head -1 | awk '{print $3}'`
 
-    echo -n "Skull $version $build_date - A fast to start, easy to maintain, "
+    echo -n "Skull $version - A fast to start, easy to maintain, "
     echo "high productivity serving framework."
     echo "More information see https://github.com/finaldie/skull"
 }
 
 function action_version_usage()
 {
-    echo "usage:"
+    echo "Usage:"
     echo "  skull version"
 }
