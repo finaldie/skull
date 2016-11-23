d94aeec6df02519e0aaccba407d79978d07cbabd
Yuzhang Hu <hyzwowtools@gmail.com>
2016-11-23 18:20:33 +0000

    Fix FT cases

--- tests/cases/py_chain_call_services/module.py
+++ tests/cases/py_chain_call_services/module.py
@@ -41,7 +41,7 @@ def module_pack(txn, txndata):
     mod_dymetrics = Metrics.transaction('test')
     mod_dymetrics.response.inc(1)
 
-    if txn.status() != Txn.Txn.TXN_OK:
+    if txn.status() != Txn.TXN_OK:
         txndata.append('error')
         Logger.error('6', 'module_pack error', 'no solution')
     else:
