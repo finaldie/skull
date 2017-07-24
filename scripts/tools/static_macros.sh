3cb8649eb4fe94c0906ed8f83befbda948c1edca
Yuzhang Hu <hyzwowtools@gmail.com>
2017-07-24 20:15:25 +0000

    Fix static_macros.sh

--- scripts/tools/static_macros.sh
+++ scripts/tools/static_macros.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 ##
 # This script is used for getting compile time static variables
@@ -8,16 +8,17 @@ FINAL_MACROS=""
 
 # based version
 version=`cat ../ChangeLog.md | head -1 | awk '{print $3}'`
-FINAL_MACROS+="-DSKULL_VERSION=\\\"$version\\\" "
+FINAL_MACROS="$FINAL_MACROS -DSKULL_VERSION=\\\"$version\\\" "
 
 # git sha1
 git_sha1=`git log -1 | head -1 | awk '{print $2}'`
 if [ $? -eq 0 ]; then
-    FINAL_MACROS+="-DSKULL_GIT_SHA1=\\\"${git_sha1}\\\" "
+    FINAL_MACROS="$FINAL_MACROS -DSKULL_GIT_SHA1=\\\"${git_sha1}\\\" "
 else
-    FINAL_MACROS+="-DSKULL_GIT_SHA1=\\\"unknown\\\" "
+    FINAL_MACROS="$FINAL_MACROS -DSKULL_GIT_SHA1=\\\"unknown\\\" "
 fi
 
 # compiler version (fetch it from builtin macros)
 
 echo "$FINAL_MACROS"
+
